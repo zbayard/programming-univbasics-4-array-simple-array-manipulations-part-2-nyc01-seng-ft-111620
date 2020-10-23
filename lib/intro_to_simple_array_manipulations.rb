99e581e4f595de8e7f322e26c8cc2a6829058914
Zach Bayard <zbayard@gmail.com>
2020-10-23 16:16:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,18 +9,21 @@ def using_insert(array , element)
 end
 
 def using_uniq(array)
-  array.uniq 
+  array.uniq
 
 end
 
 def using_flatten(array)
+  array.flatten
 
 end
 
 def using_delete(array , string)
+  array.delete(string)
 
 end
 
 def using_delete_at(array , num)
+  array.delete_at(num)
 
 end
